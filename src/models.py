c27fa538dbf3a72d5ce36fa605c16f3384f83239
Natasha <natashanewboldlondon@gmail.com>
2024-12-11 11:33:05 +0000

    Updated Streamlit app and models

--- src/models.py
+++ src/models.py
@@ -13,9 +13,9 @@ MODELS = {
     "GPT-4o mini": "gpt-4o-mini",
     "GPT-4 Turbo": "gpt-4-turbo",
     "GPT-4": "gpt-4",
-    "GPT-3.5": "gpt-3.5-turbo"
-    #"o1-preview": "o1-preview",
-    #"o1-mini": "o1-mini"
+    "GPT-3.5": "gpt-3.5-turbo",
+    "o1-preview": "o1-preview",
+    "o1-mini": "o1-mini"
     
 }
 
